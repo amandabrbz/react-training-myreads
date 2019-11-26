12d4acc6ee366e450699c04c7b6a8c3321f2167e
Amanda Barboza <amanda.brbz@hotmail.com>
2019-11-26 14:10:29 -0300

    changes
    changes code 1
    props

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,6 +1,27 @@
 import React from "react";
 import "./HomePage.css";
 
+/**
+ * PROPS
+ * {
+ * img: {src, alt}
+ * title
+ * authors
+ * }
+ */
+
+const Card = ({img, title, authors}) => {
+  return <React.Fragment>
+    <img src={img.src} alt={img.alt} />
+    <p>{title}</p>
+    <ul>
+      {authors.map((author, index) => {
+        return <li key={index}>{author}</li>
+      })}
+    </ul>
+  </React.Fragment>
+}
+
 class HomePage extends React.Component {
   render() {
     return (
@@ -10,58 +31,53 @@ class HomePage extends React.Component {
           <h3>Tô lendo</h3>
           <div className="row">
             <div className="col-md-3">
-                <img src="http://placekitten.com/200/300" alt="It's an cat"/>
-                <p>Titulo do livro</p>
-                <ul>
-                    <li>autor</li>
-                    <li>autor</li>
-                </ul>
+              <Card title="titulo1" authors={['alo', 'alo2']} img={{src: "http://placekitten.com/200/300", alt:"cat"}} />
             </div>
             <div className="col-md-3">
-                <img src="http://placekitten.com/200/300" alt="It's an cat"/>
-                <p>Titulo do livro</p>
-                <ul>
-                    <li>autor</li>
-                    <li>autor</li>
-                </ul>
+              <img src="" alt="It's an cat" />
+              <p>Titulo do livro</p>
+              <ul>
+                <li>autor</li>
+                <li>autor</li>
+              </ul>
             </div>
             <div className="col-md-3">
-                <img src="http://placekitten.com/200/300" alt="It's an cat"/>
-                <p>Titulo do livro</p>
-                <ul>
-                    <li>autor</li>
-                    <li>autor</li>
-                </ul>
+              <img src="http://placekitten.com/200/300" alt="It's an cat" />
+              <p>Titulo do livro</p>
+              <ul>
+                <li>autor</li>
+                <li>autor</li>
+              </ul>
             </div>
           </div>
           <h3>Quero ler</h3>
           <div className="row">
             <div className="col-md-3">
-                <img src="http://placekitten.com/200/300" alt="It's an cat"/>
-                <p>Titulo do livro</p>
-                <ul>
-                    <li>autor</li>
-                    <li>autor</li>
-                </ul>
+              <img src="http://placekitten.com/200/300" alt="It's an cat" />
+              <p>Titulo do livro</p>
+              <ul>
+                <li>autor</li>
+                <li>autor</li>
+              </ul>
             </div>
             <div className="col-md-3">
-                <img src="http://placekitten.com/200/300" alt="It's an cat"/>
-                <p>Titulo do livro</p>
-                <ul>
-                    <li>autor</li>
-                    <li>autor</li>
-                </ul>
+              <img src="http://placekitten.com/200/300" alt="It's an cat" />
+              <p>Titulo do livro</p>
+              <ul>
+                <li>autor</li>
+                <li>autor</li>
+              </ul>
             </div>
           </div>
           <h3>Já li</h3>
           <div className="row">
             <div className="col-md-3">
-                <img src="http://placekitten.com/200/300" alt="It's an cat"/>
-                <p>Titulo do livro</p>
-                <ul>
-                    <li>autor</li>
-                    <li>autor</li>
-                </ul>
+              <img src="http://placekitten.com/200/300" alt="It's an cat" />
+              <p>Titulo do livro</p>
+              <ul>
+                <li>autor</li>
+                <li>autor</li>
+              </ul>
             </div>
           </div>
         </div>
